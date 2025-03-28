1c77037ffd495ae038e9e5391aef3f69eedd04b5
Vadim Bakhtiarov <dima@DESKTOP-Q6TTLPS>
2025-03-28 07:49:44 +0300

    Sentry connection

--- fast_api/app.py
+++ fast_api/app.py
@@ -3,12 +3,14 @@
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
+import sentry_sdk
 from fastapi import FastAPI
 
 from fast_api.business_model.models import Base
 from fast_api.database import async_engine
 from fast_api.logs import logger
 from fast_api.register_routes import router as tasks_router
+from fast_api.secret_mission.conf_env import SENTRY_DSN
 
 
 @asynccontextmanager
@@ -22,6 +24,12 @@ async def database_life_cycle(api: FastAPI) -> AsyncIterator:
     yield
     await async_engine.dispose()
 
+# Инициализация Sentry
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+)
 
 app = FastAPI(lifespan=database_life_cycle)
 
